2b8ebf3fac411553ee8c984cbb49b7b7f0c999e7
Andrew Chavez <adchavez@gmail.com>
2013-09-17 13:30:11 -0500

    Revert "Revert "Simplified report an error markup, spelling fixes""
    This reverts commit 6417fb80688019594234555cb48b2daa2510d18e.

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -121,7 +121,7 @@ var MapsLib = {
          'Last 30 Days': [moment().subtract('days', 29), moment()],
          'This Month': [moment().startOf('month'), moment().endOf('month')],
          'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
-         'All Available Date': [moment('2012-01-01'), moment()]
+         'All Available Dates': [moment('2012-01-01'), moment()]
         },
         startDate: MapsLib.startDate,
         endDate: MapsLib.endDate,
